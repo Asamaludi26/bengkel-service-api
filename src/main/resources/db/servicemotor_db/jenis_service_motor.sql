c585c1a73c42c88f5b47021759df1f2651ab2959
Angga Samaludi <32694929+Asamaludi26@users.noreply.github.com>
2025-01-26 10:27:13 +0700

    Add db to lokal

--- src/main/resources/db/servicemotor_db/jenis_service_motor.sql
+++
@@ -1,8 +0,0 @@
-create table jenis_service_motor
-(
-    id           bigint auto_increment
-        primary key,
-    harga        double       not null,
-    nama_service varchar(255) null
-);
-
--- src/main/resources/db/servicemotor_db/jenisservicemotor.sql
+++
@@ -1,8 +0,0 @@
-create table jenisservicemotor
-(
-    id          bigint auto_increment
-        primary key,
-    namaService varchar(255) not null,
-    harga       double       not null
-);
-
